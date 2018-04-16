4b576406c770b291086fef56577d20df90737be2
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-04-16 14:53:31 +0100

    Building the Search Model - data model

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,5 @@
-import axios from 'axios';
-async function getResults(query){
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const key = "975aa925dbf676686be7b33ab0111a23";
-    try {
-    const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-    }catch(error){
-        alert(error);
-    }
-}
-getResults('tomato pasta');
+import Search from './models/Search';
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
