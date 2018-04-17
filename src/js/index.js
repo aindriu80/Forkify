31af5f7e7503a63b8848715f421554bf58b72bf6
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-04-17 15:00:28 +0100

    Search Controller - building the query

--- src/js/index.js
+++ src/js/index.js
@@ -8,9 +8,23 @@ import Search from './models/Search';
  */
 const state = {};
 
-const controlSearch = () => {
+const controlSearch = async () => {
     // 1) Get query from view
     const query = 'pizza' // TODO
+
+    if (query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes 
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result)
+    }
+
 }
 
 document.querySelector('.search').addEventListener('submit', e=> {
@@ -18,6 +32,4 @@ document.querySelector('.search').addEventListener('submit', e=> {
     controlSearch();
 });
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+
