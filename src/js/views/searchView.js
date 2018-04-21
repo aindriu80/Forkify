4ee3ebe381377750aab76c915989c9343e931aad
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-04-21 19:59:34 +0100

    Implementing Search Results - retrieving 10 items

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -51,6 +51,9 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {    
-    recipes.forEach(renderRecipe);
+export const renderResults = (recipes, page = 3, resPerPage = 5) => {    
+    const start = (page - 1) * resPerPage;
+    const end = page * resPerPage;
+
+    recipes.slice(start, end).forEach(renderRecipe);
 };
