0f07ac0438c159910b0b9f321d4324b65fcf87ea
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-01 14:51:40 +0100

    Building the Likes model

--- src/js/models/List.js
+++ src/js/models/List.js
@@ -18,7 +18,6 @@ export default class List {
 
   deleteItem(id) {
     const index = this.items.findIndex(el => el.id === id);
-
     // [2,4,8] splice(1,2) -> return [4,8], original array is [2]
     // [2,4,8] splice(1,1) -> return 4, original array is [2,4,8]
     this.items.splice(index, 1);
