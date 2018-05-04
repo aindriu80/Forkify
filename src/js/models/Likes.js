8433143de320a0d79fb6b0acbe44298ecb74fd7f
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-05-04 22:00:43 +0100

    Event listener to restore liked recipes on page load

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -33,4 +33,11 @@ export default class Likes {
   persistData() {
     localStorage.setItem('likes', JSON.stringify(this.likes));
   }
+
+  readStorage() {
+    const storage = JSON.parse(localStorage.getItem('likes'));
+    
+    // Restoring likes from the localStorage
+    if (storage) this.likes = storage;
+  }
 }
